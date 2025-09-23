1a475b45aa890b14b0104e71751d147a306c351b
su <smalfhaid@gmail.com>
2025-09-24 02:20:16 +0300

    Added functionality to the project, 3 new buttons [positive, negative, and erase], and added some style

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-function addition(x,y){
-    return x+y;
+function addition(x, y) {
+    return x + y;
 }
 
-function subtraction(x,y){
-    return x-y;
+function subtraction(x, y) {
+    return x - y;
 }
 
-function multiplication(x,y){
-    return x*y;
+function multiplication(x, y) {
+    return x * y;
 }
 
-function division(x,y){
-    return x/y;
+function division(x, y) {
+    return x / y;
 }
 
-function eq(){
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("#buttons button");
 
-}
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
 
-function ac(){
-    
-}
+        let value = button.textContent;
+
+        if (value == "AC")
+            display.textContent = "";
+        else if (value == "ERS")
+            display.textContent = display.textContent.slice(0,-1);
+        else if (value == "="){
+            let i = display.textContent.indexOf(" ");
+            let x = Number(display.textContent.slice(0,i));
+            let y = Number(display.textContent.slice(i+3,));
+
+            switch(display.textContent.slice(i+1,i+2)){
+                case "+" : value = addition(x,y); break;
+                case "-" : value = subtraction(x,y); break;
+                case "X" : value = multiplication(x,y); break;
+                case "/" : value = division(x,y); break;
+                default : value = "error";
+            }
+
+            display.textContent = value;
+        }
+        else{
+            display.textContent += value;
+        } 
+    });
+});
