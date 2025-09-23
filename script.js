93b3be7647d741a40b69ff1209893ea51fe28557
su <smalfhaid@gmail.com>
2025-09-24 00:03:16 +0300

    Added my work

---
+++ script.js
@@ -0,0 +1,23 @@
+function addition(x,y){
+    return x+y;
+}
+
+function subtraction(x,y){
+    return x-y;
+}
+
+function multiplication(x,y){
+    return x*y;
+}
+
+function division(x,y){
+    return x/y;
+}
+
+function eq(){
+
+}
+
+function ac(){
+    
+}
